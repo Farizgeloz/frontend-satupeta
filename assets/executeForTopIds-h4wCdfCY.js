2d0bf385f58e437f33d56a8525540adb050c13a4
Farizgeloz <farizgeloz26@gmail.com>
2025-10-02 02:17:33 +0700

    Updates

--- assets/executeForTopIds-h4wCdfCY.js
+++
@@ -1,1 +0,0 @@
-import{ds as m}from"./index-D-NAnen2.js";import{m as s}from"./queryTopFeatures-DaoOKMx7.js";import e from"./TopFeaturesQuery-C045JErr.js";async function n(o,r,t){const a=m(o);return(await s(a,e.from(r),{...t})).data.objectIds}export{n as executeForTopIds};
